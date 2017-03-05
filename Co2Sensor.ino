31b669d02debc0a532d7f914aa9fe30ef934c78f
Bart Theeuwes <barthe@Bart-PC.lan>
2017-03-05 14:08:57 +0100

    cleanup

--- Co2Sensor.ino
+++ Co2Sensor.ino
@@ -3,71 +3,6 @@
 
 SoftwareSerial *SwSerial;
 
-#if 0 //old
-// 9-bytes CMD PPM read command
-byte mhzCmd[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79};
-byte mhzResp[9]; // 9 bytes bytes response
-
-float getMhzValue(void)
-{
-  int i;
-  unsigned int ppm = 0;
-  byte crc = 0;
-
-  SwSerial->write(mhzCmd, 9);
-  memset(mhzResp, 0, 9);
-  SwSerial->readBytes(mhzResp, 9);
-
-  for (i = 1; i < 8; i++) 
-    crc+=mhzResp[i];
-  crc = 255 - crc;
-  crc++;
-
-  if ( !(mhzResp[0] == 0xFF && mhzResp[1] == 0x86 && mhzResp[8] == crc) ) {
-    String log = F("MHZ19: CRC error: ");
-    log += String(crc); log += " / "; log += String(mhzResp[8]);
-    Homie.getLogger() << "error: " << log << endl;
-    return (float) -1;
-  } else {
-    unsigned int mhzRespHigh = (unsigned int) mhzResp[2];
-    unsigned int mhzRespLow = (unsigned int) mhzResp[3];
-    ppm = (256 * mhzRespHigh) + mhzRespLow;
-  }
-  
-  Homie.getLogger() << "MHZ19: PPM value: " << ppm << endl;
-  
-  return (float) ppm;
-}
-#endif
-
-#if 0 //old too
-int getMhzValue()
-{
-
-  byte cmd[9] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79};
-  // command to ask for data
-  char response[9]; // for answer
-
-  SwSerial->write(cmd, 9); //request PPM CO2
-  SwSerial->readBytes(response, 9);
-  if (response[0] != 0xFF)
-  {
-    Homie.getLogger() << "Wrong starting byte from co2 sensor!" << endl;
-    return -1;
-  }
-  if (response[1] != 0x86)
-  {
-    Homie.getLogger() << "Wrong command from co2 sensor!" << endl;
-    return -1;
-  }
-
-  int responseHigh = (int) response[2];
-  int responseLow = (int) response[3];
-  int ppm = (256 * responseHigh) + responseLow;
-  return ppm;
-}
-#endif
-
 #include "mhz19.h"
 
 static bool exchange_command(uint8_t cmd, uint8_t data[], int timeout)
@@ -103,7 +38,6 @@ static bool read_temp_co2(int *co2, int *temp)
 #if 1
         char raw[32];
         sprintf(raw, "RAW: %02X %02X %02X %02X %02X %02X", data[0], data[1], data[2], data[3], data[4], data[5]);
-        //SwSerial->println(raw);
         Homie.getLogger() << raw << endl;
 #endif
     }
